196bd236bf5433780aaf7c21f35e17612fee53c6
nicolasomena <sounicolasomena@gmail.com>
2025-03-24 11:14:42 -0300

    Aula 13

---
+++ JavaScript-CursoV/ambiente.js
@@ -0,0 +1,26 @@
+let c = 1
+let numero = 20
+
+console.log(`Contando até ${numero}.`)
+
+for(c ; c<= numero; c++) {
+    console.log(c)
+}
+
+console.log('Fim')
+console.log(`${c}`)
+
+/*do {
+    console.log(`Numero ${c}`)
+    c++
+}while(c <= numero)*/
+
+
+/*let c = 1
+let numero = 20
+console.log(`Contando até ${numero}.`)
+
+while (c <= numero ) {
+    console.log(`Numero ${c}`)
+    c++
+}*/
