28edb1ef1e36a6c4765e4de65282553853a2a915
nicolasomena <sounicolasomena@gmail.com>
2025-03-24 20:44:57 -0300

    comitTreinoCalc

--- JavaScript-CursoV/Aulas/Aula014ex/ex017/script.js
+++ JavaScript-CursoV/Aulas/Aula014ex/ex017/script.js
@@ -1,4 +1,87 @@
-function gerar (){
+
+//Fazendo sozinho e comentando erros depois
+function gerar(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+//Pegar o valor com número, quase esqueci
+    n = Number(num.value)
+    c = 1
+//agora os If e else.. começar com o caso não tenha numero
+    if (n == 0){ //sinto que ta errado
+        window.alert('Digite um numero para começar')
+        tab.innerHTML = ''
+
+    }else{
+           //quase esqueci do while kkk
+           while(c < 10){
+            //esqueci como criava o elemento tive que pescar
+            item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+
+        }
+    }
+
+}
+
+
+
+//Funcionou!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function gerar (){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
 
@@ -16,4 +99,4 @@ function gerar (){
             c++
         }   
     }
-}
+}*/
